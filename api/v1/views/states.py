6ee49adbd4859dbdd9b17ea066234db371509a9d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-03 00:26:10 +0200

    add documention for module + functions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""state routes for reading, updating, creating, deleteing states
+from our storage engine"""
 from api.v1.app import app_views
 from models.state import State
 from models import storage
@@ -8,6 +10,7 @@ import json
 
 @app_views.route("/states", strict_slashes=False)
 def get_states():
+    """get all states from our storage engine"""
     objs = storage.all(State)
     objs = [obj.to_dict() for obj in objs.values()]
     return jsonify(objs)
@@ -15,6 +18,7 @@ def get_states():
 
 @app_views.route("/states/<string:state_id>", strict_slashes=False)
 def get_state(state_id):
+    """get state based on state_id"""
     obj = storage.get(State, state_id)
     if obj is None:
         return jsonify({"error": "Not found"}), 404
@@ -23,6 +27,7 @@ def get_state(state_id):
 
 @app_views.route("/states", strict_slashes=False, methods=['POST'])
 def add_state():
+    """add new state route"""
     obj = request.get_json()
     if not isinstance(obj, dict):
         return abort(Response("Not a JSON", 400))
@@ -36,6 +41,7 @@ def add_state():
 
 @app_views.route("/states/<string:state_id>", methods=["DELETE"])
 def delete_state(state_id):
+    """delete state route"""
     state = storage.get(State, state_id)
     if state is None:
         return abort(404)
@@ -46,6 +52,7 @@ def delete_state(state_id):
 
 @app_views.route("/states/<string:state_id>", methods=["PUT"])
 def update_state(state_id):
+    """update state route"""
     obj = request.get_json()
     if not isinstance(obj, dict):
         return abort(Response("Not a JSON", 400))
