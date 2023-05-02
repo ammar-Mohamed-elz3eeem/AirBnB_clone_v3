125a2a0beecf2abe0572546532196d31c67a1b85
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-02 21:12:29 +0200

    add errorhandler middleware

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,13 @@ def teardown_db(e):
         storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({
+        "error": "Not found"
+    })
+
+
 if __name__ == "__main__":
     app.run(
         threaded=True,
