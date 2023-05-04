5bc4fd83b5aa9c6d6db0cd7a3bcd96c1d084cc74
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-04 11:46:38 +0200

    update module dependencies

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,9 +2,7 @@
 """Flask api for AirBnB"""
 from flask import Flask
 from models import storage
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+from api.v1.views import *
 from os import getenv
 
 
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Flask api for AirBnB"""
 from flask import Flask, Blueprint
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
 
 
 app_views = Blueprint(
